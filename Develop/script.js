61dffc13b328f40745577f70346684626884f8cd
Mark Anthony <touya1313@hotmail.com>
2023-06-18 23:17:38 -0500

    created prompts

--- Develop/script.js
+++ Develop/script.js
@@ -27,7 +27,28 @@ function generatePassword() {
 
 }
 
+// generate password based on criteria 
+function getPrompts() {
+  characterLength = parseInt(prompt("How long will your password be? (Choose between 8 - 128 characters"));
 
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Please double check you are meeting the required criteria!");
+    return false; 
+  }
+
+  if (confirm("Would you like lowercase letters?")) {
+    choiceArray = choiceArray.concat(lowerCase);
+  }
+  if (confirm("Would you like uppercase letters?")) {
+    choiceArray = choiceArray.concat(upperCase);
+  }
+  if (confirm("Would you like special characters?")) {
+    choiceArray = choiceArray.concat(specialCharacters);
+  }
+  if (confirm("Would you like numbers?")) {
+    choiceArray = choiceArray.concat(numbers);
+  }
+}
 
 
 
