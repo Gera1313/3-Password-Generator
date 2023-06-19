8ee6b65d0234fff4aafa3d8a41fbf8b9c5184374
Mark Anthony <touya1313@hotmail.com>
2023-06-18 20:50:50 -0500

    added pseudo code

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -12,3 +12,16 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+// 1. Prompt the user for password criteria.
+//  password length between 8 to 128 characters
+//  need lowercase, uppercase, numbers, & special characters
+// 2. Validate the input
+// 3. Generate password based on criteria
+// 4. Display password to the page
